29eb8a0d875f7b5a05b93cc4f01eca731546354b
Naimur Sharon <naimur.sharon@gmail.com>
2023-03-04 21:57:42 +0600

    Added urls.py

--- chat_backend/urls.py
+++ chat_backend/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 
 
@@ -22,4 +22,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/",include('api.urls')),
     path('',TemplateView.as_view(template_name='index.html')),
+    re_path(r'^(?:.*)/?$',TemplateView.as_view(template_name='index.html')),
 ]
